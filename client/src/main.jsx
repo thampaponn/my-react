97a163958f58edf797ce8bca4081aed26da8bda3
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-05-27 19:01:19 +0700

    router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App.jsx'
 import Nav from '../src/components/Nav.jsx'
 import './index.css'
+import Root from "./routes/root.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Nav />
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
