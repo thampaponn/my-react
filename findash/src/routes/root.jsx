09927af4c82c33d3c0430ec025c4ea65a4deef64
thampaponn <88426179+thampaponn@users.noreply.github.com>
2023-06-02 01:17:10 +0700

    update

---
+++ findash/src/routes/root.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+function Root() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div>
+                
+            </div>
+        </>
+    )
+}
+
+export default Root
